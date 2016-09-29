dad36d2787615c98bcce1bea5967443ca02d85dc
jiiun <511157871@qq.com>
2016-09-29 17:28:32 +0800

    数据缓存，包括DOM节点上的数据

---
+++ src/modules/cache.js
@@ -0,0 +1,284 @@
+var objectUtil = require('./object');
+var arrayUtil = require('./array');
+var transferUtil = require('./transfer');
+var eventHandler = require('./eventHandler');
+
+var deletedIds = [];
+
+var internalData = function ( elem, name, data, pvt /* Internal Use Only */ ) {
+	if ( !Cache.acceptData( elem ) ) {
+		return;
+	}
+	var ret, thisCache,
+		internalKey = Cache.expando,
+
+		// We have to handle DOM nodes and JS objects differently because IE6-7
+		// can't GC object references properly across the DOM-JS boundary
+		isNode = elem.nodeType,
+
+		// Only DOM nodes need the global jQuery cache; JS object data is
+		// attached directly to the object so GC can occur automatically
+		cache = isNode ? Cache.space : elem,
+
+		// Only defining an ID for JS objects if its cache already exists allows
+		// the code to shortcut on the same path as a DOM node with no cache
+		id = isNode ? elem[ internalKey ] : elem[ internalKey ] && internalKey;
+
+	// Avoid doing any more work than we need to when trying to get data on an
+	// object that has no data at all
+	if ( (!id || !cache[id] || (!pvt && !cache[id].data)) && data === undefined && typeof name === "string" ) {
+		return;
+	}
+
+	if ( !id ) {
+		// Only DOM nodes need a new unique ID for each element since their data
+		// ends up in the global cache
+		if ( isNode ) {
+			id = elem[ internalKey ] = deletedIds.pop() || Cache.guid++;
+		} else {
+			id = internalKey;
+		}
+	}
+
+	if ( !cache[ id ] ) {
+		// Avoid exposing jQuery metadata on plain JS objects when the object
+		// is serialized using JSON.stringify
+		cache[ id ] = isNode ? {} : { toJSON: function(){} };
+	}
+
+	// An object can be passed to jQuery.data instead of a key/value pair; this gets
+	// shallow copied over onto the existing cache
+	if ( typeof name === "object" || typeof name === "function" ) {
+		if ( pvt ) {
+			cache[ id ] = objectUtil.extend( cache[ id ], name );
+		} else {
+			cache[ id ].data = objectUtil.extend( cache[ id ].data, name );
+		}
+	}
+
+	thisCache = cache[ id ];
+
+	// jQuery data() is stored in a separate object inside the object's internal data
+	// cache in order to avoid key collisions between internal data and user-defined
+	// data.
+	if ( !pvt ) {
+		if ( !thisCache.data ) {
+			thisCache.data = {};
+		}
+
+		thisCache = thisCache.data;
+	}
+
+	if ( data !== undefined ) {
+		thisCache[ transferUtil.camelCase( name ) ] = data;
+	}
+
+	// Check for both converted-to-camel and non-converted data property names
+	// If a data property was specified
+	if ( typeof name === "string" ) {
+
+		// First Try to find as-is property data
+		ret = thisCache[ name ];
+
+		// Test for null|undefined property data
+		if ( ret == null ) {
+
+			// Try to find the camelCased property
+			ret = thisCache[ transferUtil.camelCase( name ) ];
+		}
+	} else {
+		ret = thisCache;
+	}
+
+	return ret;
+}
+
+function internalRemoveData( elem, name, pvt ) {
+	if ( !Cache.acceptData( elem ) ) {
+		return;
+	}
+
+	var thisCache, i,
+		isNode = elem.nodeType,
+
+		// See jQuery.data for more information
+		cache = isNode ? Cache.space : elem,
+		id = isNode ? elem[ Cache.expando ] : Cache.expando;
+
+	// If there is already no cache entry for this object, there is no
+	// purpose in continuing
+	if ( !cache[ id ] ) {
+		return;
+	}
+
+	if ( name ) {
+
+		thisCache = pvt ? cache[ id ] : cache[ id ].data;
+
+		if ( thisCache ) {
+
+			// Support array or space separated string names for data keys
+			if ( !arrayUtil.isArray( name ) ) {
+
+				// try the string as a key before any manipulation
+				if ( name in thisCache ) {
+					name = [ name ];
+				} else {
+
+					// split the camel cased version by spaces unless a key with the spaces exists
+					name = transferUtil.camelCase( name );
+					if ( name in thisCache ) {
+						name = [ name ];
+					} else {
+						name = name.split(" ");
+					}
+				}
+			} else {
+				// If "name" is an array of keys...
+				// When data is initially created, via ("key", "val") signature,
+				// keys will be converted to camelCase.
+				// Since there is no way to tell _how_ a key was added, remove
+				// both plain key and camelCase key. #12786
+				// This will only penalize the array argument path.
+				name = name.concat( arrayUtil.map( name, transferUtil.camelCase ) );
+			}
+
+			i = name.length;
+			while ( i-- ) {
+				delete thisCache[ name[i] ];
+			}
+
+			// If there is no data left in the cache, we want to continue
+			// and let the cache object itself get destroyed
+			if ( pvt ? !isEmptyDataObject(thisCache) : !objectUtil.isEmptyObject(thisCache) ) {
+				return;
+			}
+		}
+	}
+
+	// See jQuery.data for more information
+	if ( !pvt ) {
+		delete cache[ id ].data;
+
+		// Don't destroy the parent cache unless the internal data object
+		// had been the only thing left in it
+		if ( !isEmptyDataObject( cache[ id ] ) ) {
+			return;
+		}
+	}
+
+	// Destroy the cache
+	if ( isNode ) {
+		cleanData( [ elem ], true );
+
+	// Use delete when supported for expandos or `cache` is not a window per isWindow (#10080)
+	/* jshint eqeqeq: false */
+	} else if ( support.deleteExpando || cache != cache.window ) {
+		/* jshint eqeqeq: true */
+		delete cache[ id ];
+
+	// When all else fails, null
+	} else {
+		cache[ id ] = null;
+	}
+}
+
+var cleanData = function( elems, /* internal */ acceptData ) {
+	var elem, type, id, data,
+		i = 0,
+		internalKey = Cache.expando,
+		cache = Cache.space,
+		deleteExpando = support.deleteExpando,
+		special = eventHandler.event.special;
+
+	for ( ; (elem = elems[i]) != null; i++ ) {
+		if ( acceptData || Cache.acceptData( elem ) ) {
+
+			id = elem[ internalKey ];
+			data = id && cache[ id ];
+
+			if ( data ) {
+				if ( data.events ) {
+					for ( type in data.events ) {
+						if ( special[ type ] ) {
+							eventHandler.event.remove( elem, type );
+
+						// This is a shortcut to avoid jQuery.event.remove's overhead
+						} else {
+							eventHandler.removeEvent( elem, type, data.handle );
+						}
+					}
+				}
+
+				// Remove cache only if it was not already removed by jQuery.event.remove
+				if ( cache[ id ] ) {
+
+					delete cache[ id ];
+
+					// IE does not allow us to delete expando properties from nodes,
+					// nor does it have a removeAttribute function on Document nodes;
+					// we must handle all of these cases
+					if ( deleteExpando ) {
+						delete elem[ internalKey ];
+
+					} else if ( typeof elem.removeAttribute !== strundefined ) {
+						elem.removeAttribute( internalKey );
+
+					} else {
+						elem[ internalKey ] = null;
+					}
+
+					deletedIds.push( id );
+				}
+			}
+		}
+	}
+}
+
+var Cache = {
+	expando: 'unique' + Math.random().toString().slice(2),
+	guid: 1,
+	space: {},
+	noData: {
+		"applet ": true,
+		"embed ": true,
+		// ...but Flash objects (which have this classid) *can* handle expandos
+		"object ": "clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"
+	},
+
+	hasData: function( elem ) {
+		elem = elem.nodeType ? Cache.space[ elem[Cache.expando] ] : elem[ Cache.expando ];
+		return !!elem && !isEmptyDataObject( elem );
+	},
+
+	data: function( elem, name, data ) {
+		return internalData( elem, name, data );
+	},
+
+	removeData: function( elem, name ) {
+		return internalRemoveData( elem, name );
+	},
+
+	// For internal use only.
+	_data: function( elem, name, data ) {
+		return internalData( elem, name, data, true );
+	},
+
+	_removeData: function( elem, name ) {
+		return internalRemoveData( elem, name, true );
+	},
+
+	acceptData: function( elem ) {
+		var noData = Cache.noData[ (elem.nodeName + " ").toLowerCase() ],
+			nodeType = +elem.nodeType || 1;
+
+		// Do not set data on non-element DOM nodes because it will not be cleared (#8335).
+		return nodeType !== 1 && nodeType !== 9 ?
+			false :
+
+			// Nodes accept data unless otherwise specified; rejection can be conditional
+			!noData || noData !== true && elem.getAttribute("classid") === noData;
+	}
+}
+
+module.exports = Cache;
